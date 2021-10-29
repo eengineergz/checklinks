ed7270f9067fa55001ee783bb3731ca7a19471d2
bryan <bryan.guner@gmail.com>
2021-10-28 21:50:43 -0400

    formatting

--- Check-My-Links-master/options.js
+++ Check-My-Links-master/options.js
@@ -3,12 +3,12 @@ _gaq.push(['_setAccount', 'UA-98457349-3']);
 _gaq.push(['_trackPageview']);
 
 (() => {
-  const ga = document.createElement('script');
-  ga.type = 'text/javascript';
-  ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  const s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(ga, s);
+    const ga = document.createElement( 'script' );
+    ga.type = 'text/javascript';
+    ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    const s = document.getElementsByTagName( 'script' )[ 0 ];
+    s.parentNode.insertBefore( ga, s );
 })();
 
 function loadOptions() {
